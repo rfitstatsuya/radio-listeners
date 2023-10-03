dcd522beee63fff5a4db6c3ca95b270b2631ab17
rfitstatsuya <142726441+rfitstatsuya@users.noreply.github.com>
2023-10-03 14:40:27 +0900

    ユーザー情報削除機能　is_deletedカラム　追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_02_063515) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_03_024604) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -49,6 +49,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_02_063515) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_deleted", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
