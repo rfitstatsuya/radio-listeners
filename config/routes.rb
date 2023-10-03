3bad7cabe108c3acd233eebaf74fd49800f5bd17
rfitstatsuya <142726441+rfitstatsuya@users.noreply.github.com>
2023-10-03 11:42:29 +0900

    ユーザー情報編集　ルーティング設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: "users/registrations",
+    sessions: "users/sessions",
+    passwords: "users/passwords",
+    confirmations: "users/confirmations"
+  }
   root to: 'radios#index'
   resources :radios, only: :index
-  resources :users, only: :show
+  resources :users, only: [:show, :edit, :update]
 end
