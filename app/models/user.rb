3f1d4b70068abb06ac78956d08c77b04f2d150a5
rfitstatsuya <142726441+rfitstatsuya@users.noreply.github.com>
2023-10-03 11:43:07 +0900

    ユーザー情報編集　モデル設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,22 @@ class User < ApplicationRecord
 
         with_options presence: true do
           validates :radio_name
+          validates :password, on: :create
         end
       
+        validates :profile, length: { maximum: 140 }
+
         VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
         validates :password, format: { with: VALID_PASSWORD_REGEX }
+
+        def update_without_current_password(params, *options)
+          if params[:password].blank? && params[:password_confirmation].blank?
+            params.delete(:password)
+            params.delete(:password_confirmation)
+          end
+      
+          result = update(params, *options)
+          clean_up_passwords
+          result
+        end
 end
