328fc61c8dc18215533e31571f88757c09c0943d
rfitstatsuya <142726441+rfitstatsuya@users.noreply.github.com>
2023-10-08 16:49:54 +0900

    messageモデル・テーブル　作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_05_090549) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_08_074636) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -46,6 +46,16 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_05_090549) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "messages", charset: "utf8", force: :cascade do |t|
+    t.string "content", null: false
+    t.bigint "radio_id", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["radio_id"], name: "index_messages_on_radio_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "radios", charset: "utf8", force: :cascade do |t|
     t.bigint "category_id", null: false
     t.datetime "created_at", null: false
@@ -70,5 +80,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_05_090549) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "messages", "radios"
+  add_foreign_key "messages", "users"
   add_foreign_key "radios", "categories"
 end
