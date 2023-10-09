19dfca30e56c8109718d724f4d14601bb2dafa9c
rfitstatsuya <142726441+rfitstatsuya@users.noreply.github.com>
2023-10-09 17:23:04 +0900

    メッセージ削除機能　コントローラー編集

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -16,6 +16,19 @@ class MessagesController < ApplicationController
     end
   end
 
+  def destroy
+    @radio = Radio.find(params[:radio_id])
+    @message = @radio.messages.find(params[:id])
+    if current_user.id == @message.user.id
+      @message.destroy
+      flash[:notice] = 'コメントを削除しました'
+      redirect_to radio_messages_path(@radio)
+    else
+      @messages = @room.messages.includes(:user)
+      render :index, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def message_params
